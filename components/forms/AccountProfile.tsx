c55193e1033261cdd834873a7a0770584c6ed24a
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-10-04 17:11:16 +0100

    add user update/create

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { ChangeEvent, useState } from "react"
+import { usePathname, useRouter } from "next/navigation"  
 import Image from "next/image"
 
 import { useForm } from "react-hook-form"
@@ -9,6 +10,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { UserValidation } from "@/lib/validations/user"
 import { isBase64Image } from "@/lib/utils"
 import { useUploadThing } from "@/lib/uploadthing"
+import { updateUser } from "@/lib/actions/user.actions"
 
 import {
     Form,
@@ -38,6 +40,9 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
     const [files, setFiles] = useState<File[]>([])
     const { startUpload } = useUploadThing("media")
+
+    const router = useRouter()
+    const pathname = usePathname()
 
 
     const form = useForm({
@@ -91,7 +96,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             }
         }
 
-        // TODO: Update user profile
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        })
+
+        if (pathname === '/profile/edit') {
+            router.back()
+        } else {
+            router.push('/')
+        }
     }
 
 
