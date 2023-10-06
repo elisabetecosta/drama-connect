a2e84c05730b8769a58f378d5ee8b418a20c40f7
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-10-06 12:29:17 +0100

    add activity page

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,10 @@
+async function Page() {
+
+    return (
+        <section>
+            <h1 className="head-text mb-10">Communities</h1>
+        </section>
+    )
+}
+
+export default Page
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -16,6 +16,7 @@ interface Params {
     path: string;
 }
 
+
 export async function updateUser({
     userId,
     username,
@@ -65,7 +66,7 @@ export async function fetchUser(userId: string) {
         // })
 
     } catch (error: any) {
-        throw new Error(`Failed to fetch user: ${error.message}`)
+        throw new Error(`Failed to fetch user: ${error.message}`);
     }
 }
 
@@ -95,7 +96,7 @@ export async function fetchUserPosts(userId: string) {
         return threads;
 
     } catch (error: any) {
-        throw new Error(`Failed to fetch user posts: ${error.message}`)
+        throw new Error(`Failed to fetch user posts: ${error.message}`);
     }
 }
 
@@ -149,6 +150,37 @@ export async function fetchUsers({
             return { users, isNext };
 
     } catch (error: any) {
-        throw new Error(`Failed to fetch users: ${error.message}`)
+        throw new Error(`Failed to fetch users: ${error.message}`);
+    }
+}
+
+
+export async function getActivity(userId: string) {
+
+    try {
+        
+        connectToDB();
+
+        // Find all threads created by the user
+        const userThreads = await Thread.find({ author: userId });
+
+        // Collect all the child thread ids (replies) from the 'children' field
+        const childThreadIds = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children)
+        }, []);
+
+        const replies = await Thread.find({
+            _id: { $in: childThreadIds },
+            author: { $ne: userId }
+        }).populate({
+            path: 'author',
+            model: User,
+            select: "name image _id"
+        });
+
+        return replies;
+
+    } catch (error: any) {
+        throw new Error(`Failed to fetch activity: ${error.message}`);
     }
 }
